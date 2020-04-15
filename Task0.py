5e0e2ab7496cdcc55ef0b5017292e5a93a9b76be
Karthikeya Reddy Vasipalli <reddyvasipalli98@outlook.com>
2020-04-15 08:19:13 -0500

    sample commit

--- Task0.py
+++ Task0.py
@@ -1,6 +1,6 @@
 """
 Read file into texts and calls.
-It's ok if you don't understand how to read files.
+It's ok, if you don't understand how to read files.
 """
 import csv
 with open('texts.csv', 'r') as f:
